caaf76b6993213f08e42e119ff04573b0df82d68
Simon Holt <simonholt83@gmail.com>
2014-10-14 13:44:10 +0200

    jsonp enabled the get-ting-object ajax callback

--- js/ting_visual_relation.field.js
+++ js/ting_visual_relation.field.js
@@ -1,6 +1,23 @@
 (function($) {
+
   "use strict";
   
-  
+  Drupal.behaviors.tingVisualRelation = {
+  	attach: function(context, settings) {
+  		// Test jsonp:
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = '/ting-visual-relation/get-ting-object/870970-basis:26917921?callback=test';
+			var head = document.getElementsByTagName('head')[0];
+			head.appendChild(script);
+  		/*$.ajax({
+  			// for test: 870970-basis:26917921
+  			url: "/ting-visual-relation/get-ting-object",
+  			success: function(data) {
+  				alert("success");
+  			}
+  		}); */
+  	}
+  }
 
 }(jQuery));
