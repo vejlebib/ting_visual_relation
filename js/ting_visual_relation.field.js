b18fcc64dc5da47f65056547bf6c596c097a883d
Simon Holt <simonholt83@gmail.com>
2014-10-15 16:27:05 +0200

    Added to jsonp ajax method. The method is now returning data about title only

--- js/ting_visual_relation.field.js
+++ js/ting_visual_relation.field.js
@@ -7,11 +7,12 @@
   		// Test jsonp:
 			var script = document.createElement('script');
 			script.type = 'text/javascript';
+      //script.src = '/ting-visual-relation/get-ting-object/870971-tsart:84419214?callback=test';
 			script.src = '/ting-visual-relation/get-ting-object/870970-basis:26917921?callback=test';
 			var head = document.getElementsByTagName('head')[0];
 			head.appendChild(script);
+      // Normal ajax call test:
   		/*$.ajax({
-  			// for test: 870970-basis:26917921
   			url: "/ting-visual-relation/get-ting-object",
   			success: function(data) {
   				alert("success");
