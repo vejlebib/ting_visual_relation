2c0304c1a3b295d988f3dcde6584e922d3dcdd76
Simon Holt <simonholt83@gmail.com>
2014-12-18 16:53:39 +0100

    Added implementation of ting_visual_relation_activation_area template.

--- ting-visual-relation-activation-area.tpl.php
+++ ting-visual-relation-activation-area.tpl.php
@@ -4,5 +4,23 @@
  * Default implementation of the ting-visual-relation-activation-arae template.
  *
  * Available variables:
- *
+ *   - $activators: The relation browser activators to be renderes in this
+ *     activation area.
+ *   - $attributes: String of attributes that can be used to style contextually
+ *     with CSS. It can be manipulated throught $classes_array variable in
+ *     preprocess functions.
+ *   - $classes: String of classes that can be used to style contextually with
+ *     CSS. It can be manipulated throught $classes_array variable in preprocess
+ *     functions.
  */
+?>
+
+<?php if (!empty($activators): ?>
+  <div class="ting-visual-relation-wrapper <?php print $classes?>" <?php print $attributes; ?>>
+    <div class="ting-visual-relation-innner-wrapper">
+      <?php foreach ($activators as $activator): ?>
+        <?php print render($activator); ?>
+      <?php endforeach; ?>
+    </div>
+  </div>
+<?php endif; ?>
