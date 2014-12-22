9a6c5223b2039326882dcf6bfb5568a5c23c52e0
Simon Holt <simonholt83@gmail.com>
2014-12-22 10:31:26 +0100

    Removed unused test code.

--- js/ting_visual_relation.js
+++ js/ting_visual_relation.js
@@ -4,20 +4,7 @@
 
   Drupal.behaviors.tingVisualRelation = {
   	attach: function(context, settings) {
-  		// Test jsonp:
-			var script = document.createElement('script');
-			script.type = 'text/javascript';
-      //script.src = '/ting-visual-relation/get-ting-object/870971-tsart:84419214?callback=test';
-			script.src = '/ting-visual-relation/get-ting-object/870970-basis:26917921?callback=test';
-			var head = document.getElementsByTagName('head')[0];
-			head.appendChild(script);
-      // Normal ajax call test:
-  		/*$.ajax({
-  			url: "/ting-visual-relation/get-ting-object",
-  			success: function(data) {
-  				alert("success");
-  			}
-  		}); */
+
   	}
   }
 
