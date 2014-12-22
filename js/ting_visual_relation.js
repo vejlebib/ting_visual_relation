85689a1896b60a81d0cbb01cbe1f0eb881afff06
Simon Holt <simonholt83@gmail.com>
2014-12-22 13:26:54 +0100

    Added initialization of relationsbrowser to attach function.

--- js/ting_visual_relation.js
+++ js/ting_visual_relation.js
@@ -4,7 +4,13 @@
 
   Drupal.behaviors.tingVisualRelation = {
   	attach: function(context, settings) {
-
+      relvis.init({
+        apiUrl: Drupal.settings.tingVisualRelation.apiUrl,
+        logUrl: Drupal.settings.tingVisualRelation.logUrl,
+        clickHandle: function(o) {
+          console.log('click', o);
+        }
+      });
   	}
   }
 
