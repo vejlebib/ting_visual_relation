fba76b333d636182a0a1345f58e36e1b60b93527
Simon Holt <simonholt83@gmail.com>
2014-12-22 16:38:18 +0100

    Added to relation browser clickHandle function. Open a new window/tab with datawell object being cliked on.

--- js/ting_visual_relation.js
+++ js/ting_visual_relation.js
@@ -9,6 +9,17 @@
         logUrl: settings.tingVisualRelation.logUrl,
         clickHandle: function(o) {
           console.log('click', o);
+          if (typeof(o.id) !== 'undefined') {
+            var a = document.createElement('a');
+            a.href = '/ting/object/' + o.id;
+            a.target = '_blank';
+            var evt = new window.MouseEvent('click', {
+              view: window,
+              bubbles: true,
+              cancelable: true
+            });
+            a.dispatchEvent(evt);
+          }
         }
       });
   	}
