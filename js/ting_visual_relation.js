85a6f7b31435438e86ecdabf7e52be817bea0deb
Simon Holt <simonholt83@gmail.com>
2014-12-23 13:39:49 +0100

    Use window.open instead of virtual anchor element.

--- js/ting_visual_relation.js
+++ js/ting_visual_relation.js
@@ -8,17 +8,8 @@
         apiUrl: settings.tingVisualRelation.apiUrl,
         logUrl: settings.tingVisualRelation.logUrl,
         clickHandle: function(o) {
-          console.log('click', o);
           if (typeof(o.id) !== 'undefined') {
-            var a = document.createElement('a');
-            a.href = '/ting/object/' + o.id;
-            a.target = '_blank';
-            var evt = new window.MouseEvent('click', {
-              view: window,
-              bubbles: true,
-              cancelable: true
-            });
-            a.dispatchEvent(evt);
+            window.open('/ting/object/' + o.id);
           }
         }
       });
