53be9d1330e9ed955a5e4c2590b38d44a35e6ba3
Simon Holt <simonholt83@gmail.com>
2014-12-23 15:27:32 +0100

    Updated relvis clickHandle. If the clicked element has a datawell-pid: Show the ting_object. Otherwise: Make a search with term or name.

--- js/ting_visual_relation.js
+++ js/ting_visual_relation.js
@@ -9,7 +9,17 @@
         logUrl: settings.tingVisualRelation.logUrl,
         clickHandle: function(o) {
           if (typeof(o.id) !== 'undefined') {
-            window.open('/ting/object/' + o.id);
+            // Regex that matches a datawell-pid
+            var regex = /^\d{6}-{1}\D+:{1}\d+$/;
+            // If this a datawell object: Show it
+            if (regex.test(o.id)) {
+              window.open('/ting/object/' + o.id);
+            }
+            // Otherwise it's a term or a name: Use it in a search
+            // TODO: Should use term.subject or term.author in search.
+            else {
+              window.open('/search/ting/' + o.id);
+            }
           }
         }
       });
